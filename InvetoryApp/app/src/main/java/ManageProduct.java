acdde140f9ec787b43e0fbc58ea932c901e06e0b
James Peake <peakejw@vcu.edu>
2018-04-02 01:22:42 -0400

    added manage profile

---
+++ InvetoryApp/app/src/main/java/ManageProduct.java
@@ -0,0 +1,18 @@
+import android.app.Application;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import e.wyattpeake.invetoryapp.R;
+
+/**
+ * Created by wyattpeake on 4/2/18.
+ */
+
+public class ManageProduct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.manage_product);
+    }
+}
