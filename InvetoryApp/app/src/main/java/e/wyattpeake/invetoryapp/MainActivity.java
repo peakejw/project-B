a5fb83ebbcd627a455f50e63f954ec3ce8491486
James Peake <peakejw@vcu.edu>
2018-04-02 01:56:56 -0400

    corrected some bugs

--- InvetoryApp/app/src/main/java/e/wyattpeake/invetoryapp/MainActivity.java
+++ InvetoryApp/app/src/main/java/e/wyattpeake/invetoryapp/MainActivity.java
@@ -1,7 +1,9 @@
 package e.wyattpeake.invetoryapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onButtonClick (View v){
+
+        if(v.getId() == R.id.Blogin){
+            Intent i = new Intent(MainActivity.this, ManageProducts.class);
+            startActivity(i);
+        }
+
     }
 }
--- InvetoryApp/app/src/main/java/e/wyattpeake/invetoryapp/ManageProducts.java
+++ InvetoryApp/app/src/main/java/e/wyattpeake/invetoryapp/ManageProducts.java
@@ -1,15 +1,13 @@
-import android.app.Application;
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+package e.wyattpeake.invetoryapp;
 
-import e.wyattpeake.invetoryapp.R;
+import android.app.Activity;
+import android.os.Bundle;
 
 /**
  * Created by wyattpeake on 4/2/18.
  */
 
-public class ManageProduct extends AppCompatActivity {
-
+public class ManageProducts extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
