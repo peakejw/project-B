ae71dfe0ab38033d5794e0f46d337a1fa6be74cf
Vasu Dhingani <vasudhingani@gmail.com>
2018-04-03 16:46:18 -0400

    first commit

--- InvetoryApp/app/src/main/java/e/wyattpeake/invetoryapp/MainActivity.java
+++ InvetoryApp/app/src/main/java/e/wyattpeake/invetoryapp/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
-    //vasu 
+    //vasu
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
