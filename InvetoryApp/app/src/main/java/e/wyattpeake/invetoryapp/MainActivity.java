ff5c8676580515967b3e2dda4372fe82e08b0950
James Peake <peakejw@vcu.edu>
2018-04-03 16:42:46 -0400

    message

--- InvetoryApp/app/src/main/java/e/wyattpeake/invetoryapp/MainActivity.java
+++ InvetoryApp/app/src/main/java/e/wyattpeake/invetoryapp/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
-    //vasu 
+    //vasu and wyatt
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
