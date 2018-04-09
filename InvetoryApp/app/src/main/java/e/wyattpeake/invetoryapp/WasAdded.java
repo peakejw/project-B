27c631937f402f9aa2b689cd38189b116765668e
James Peake <peakejw@vcu.edu>
2018-04-08 21:49:02 -0400

    somewhat finished "was added"

--- InvetoryApp/app/src/main/java/e/wyattpeake/invetoryapp/WasAdded.java
+++ InvetoryApp/app/src/main/java/e/wyattpeake/invetoryapp/WasAdded.java
@@ -1,13 +1,34 @@
 package e.wyattpeake.invetoryapp;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class WasAdded extends AddProduct  {
+public class WasAdded extends Activity {
     DataBaseHelper helper = new DataBaseHelper(this);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.was_added);
+        String search =  getIntent().getStringExtra("product");
+        Product product = new Product();
+       product =  helper.search(search);
+
+        TextView tv = (TextView)findViewById(R.id.TVidAdd);
+        tv.setText(product.getId());
+
+         tv = (TextView)findViewById(R.id.TVproductAdd);
+        tv.setText(product.getProductName());
+
+         tv = (TextView)findViewById(R.id.TVquantityAdd);
+        tv.setText(product.getQuantity());
+
+         tv = (TextView)findViewById(R.id.TVPriceAdd);
+        tv.setText(product.getPrice());
+
+
     }
+
+
 
 }
