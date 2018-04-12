7c56cda05ba0f63d2b6cf62b34e5887222bc627a
Vasu Dhingani <vasudhingani@gmail.com>
2018-04-12 16:08:23 -0400

    Vasu's screens were added

--- InvetoryApp/app/src/main/java/e/wyattpeake/invetoryapp/ManageorView.java
+++ InvetoryApp/app/src/main/java/e/wyattpeake/invetoryapp/ManageorView.java
@@ -1,7 +1,9 @@
 package e.wyattpeake.invetoryapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ManageorView extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class ManageorView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manageor_view);
+    }
+
+
+    public void onButtonClick (View v){
+
+        if(v.getId() == R.id.SignInButton){
+            Intent i = new Intent(ManageorView.this, AddProduct.class);
+            startActivity(i);
+        }
+
     }
 }
