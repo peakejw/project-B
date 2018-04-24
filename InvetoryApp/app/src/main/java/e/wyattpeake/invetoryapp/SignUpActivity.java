b766ffb8f5163e33a6703e0f5da69d8363c8d256
James Peake <peakejw@vcu.edu>
2018-04-24 16:30:09 -0400

    worked on sign up

--- InvetoryApp/app/src/main/java/e/wyattpeake/invetoryapp/SignUpActivity.java
+++ InvetoryApp/app/src/main/java/e/wyattpeake/invetoryapp/SignUpActivity.java
@@ -1,8 +1,10 @@
 package e.wyattpeake.invetoryapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -12,33 +14,43 @@ public class SignUpActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-
-        String confirm;
-
-
-        PasswordDataBase helper = new PasswordDataBase(this);
-        UserName userName = new UserName();
-
-        EditText a = (EditText) findViewById(R.id.TfUsername);
-        String b = a.getText().toString();
-        userName.setUserName(b);
-
-        while(userName.getPassword() == null) {
-
-            a = (EditText) findViewById(R.id.TFPasword);
-            b = a.getText().toString();
-            a = (EditText) findViewById(R.id.TFConfirm);
-            confirm = a.getText().toString();
-
-            if (confirm.equals(b)) {
-                userName.setPassword(b);
-            }
-        }
-
-        helper.insertUser(userName);
-
-
-
-
     }
+
+    public void onButtonClick(View view) {
+
+        if (view.getId() == R.id.Bcreate) {
+
+            String confirm;
+
+
+            PasswordDataBase helper = new PasswordDataBase(this);
+            UserName userName = new UserName();
+
+            EditText a = (EditText) findViewById(R.id.TfUsername);
+            String b = a.getText().toString();
+            userName.setUserName(b);
+
+            while (userName.getPassword() == null) {
+
+                a = (EditText) findViewById(R.id.TFPasword);
+                b = a.getText().toString();
+                a = (EditText) findViewById(R.id.TFConfirm);
+                confirm = a.getText().toString();
+
+                if (confirm.equals(b)) {
+                    userName.setPassword(b);
+                }
+            }
+
+            helper.insertUser(userName);
+
+            Intent i = new Intent(SignUpActivity.this, LogInActivity.class);
+            startActivity(i);
+        }
+    }
+
+
+
+
+
 }
